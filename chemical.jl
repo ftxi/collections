72bb3efa9ae2d414ca36f749e5f2d24df4cfd88b
sclereid <1307689960@qq.com>
2017-04-20 21:04:58 +0800

    Update chemical.jl

--- chemical.jl
+++ chemical.jl
@@ -1,8 +1,8 @@
+#!usr/bin/env julia
 
+str = "CH4 K3[Fe(CN)6] C(NH2)3CH(CH3)2"
 
-str = "CH4 K3[Fe(SCN)6]"
-
-function nlindep{T}(args::Vector{T}...)
+function lindep{T}(args::Vector{T}...)
     length(args) > length(args[1]) && false
     rank(hcat(args...)) == length(args)
 end
@@ -13,9 +13,15 @@ for elemdata in split(str)
     flag = true
     while flag
         flag = false
-        m = match(r".*\((.*?)\)([0-9]*)", elemdata)
-        m != nothing && (flag=true; k = m[2]=="" ? 1 : parse(Int, m[2]))
-        replace(elemdata, r".*?(\(.*?\)[0-9]*)",  m[1]^k)
+        m = match(r".*?\((.*?)\)([0-9]*)", elemdata)
+        m != nothing && ((flag=true; k = m[2]=="" ? 1 : parse(Int, m[2]));
+            elemdata = replace(elemdata, r"(\(.*?\)[0-9]*)",  m[1]^k, 1))
+        m = match(r".*?\[(.*?)\]([0-9]*)", elemdata)
+        m != nothing && ((flag=true; k = m[2]=="" ? 1 : parse(Int, m[2]));
+            elemdata = replace(elemdata, r"(\[.*?\][0-9]*)",  m[1]^k, 1))
+        m = match(r".*?\{(.*?)\}([0-9]*)", elemdata)
+        m != nothing && ((flag=true; k = m[2]=="" ? 1 : parse(Int, m[2]));
+            elemdata = replace(elemdata, r"(\{.*?\}[0-9]*)",  m[1]^k, 1))
     end
     println(elemdata)
 end
