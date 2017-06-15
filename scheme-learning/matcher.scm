c3faf135a75157bd0f5ccdb0786a9bc84f3c116b
ss <1307689960@qq.com>
2017-06-15 10:09:29 +0800

    create matcher.scm

---
+++ scheme-learning/matcher.scm
@@ -0,0 +1,88 @@
+#lang scheme
+
+(define (atom? x)
+  (and (not (pair? x))
+       (not (null? x))))
+
+(define deriv-rules
+  '(
+    ((dd (?c c) (?v v)) 0)
+    ((dd (?v v) (?v v)) 1)
+    ((dd (?v u) (?v v)) 0)
+    ((dd (+ (? x1) (? x2)) (?v v))
+     (+ (dd (: x1) (: v)) (dd (: x2) (: v))))
+    ((dd (* (? x1) (? x2)) (?v v))
+     (+ (* (dd (: x1) (: v)) (: x2))
+        (* (dd (: x2) (: v)) (: x1))))))
+
+#|
+(define algebra-rules
+  '(
+    (((? op) (?c c1) (?c c2)) (: (op c1 c2)))
+    (((? op) (? e) (?c c)) ((: op) (: c) (: e)))
+|#
+
+(define (match pat exp dict)
+  (cond
+    ((eq? dict 'failed) 'failed)
+    ((atom? pat)
+     (if (and (atom? exp)
+              (eq? pat exp))
+         dict
+         'failed))
+    ((null? pat)
+     dict)
+    ((arbitary-constant? pat)
+     (if (constant? exp)
+         (extend-dict pat exp dict)
+         'failed))
+    ((arbitary-variable? pat)
+     (if (variable? exp)
+         (extend-dict pat exp dict)
+         'failed))
+    ((arbitary-expression? pat)
+     (extend-dict pat exp dict))
+    ((atom? exp)
+     'failed)
+    (else
+     (match (cdr pat)
+            (cdr exp)
+            (match (car pat)
+                   (car exp)
+                   dict)))))
+
+(define (instantiate skeleton dict)
+  (define (sub-instantiate s)
+    (cond
+      ((atom? s) s)
+      ((skeleton-evaluation? s)
+       (skeleton-evaluate s dict))
+      (else (cons (sub-instantiate (car s))
+                  (sub-instantiate (cdr s))))))
+  (sub-instantiate skeleton))
+
+(define (extend-dict pat exp dict)
+  (cons (cons (cadr pat) exp)
+        dict))
+
+(define (arbitary-constant? pat)
+  (eq? (car pat) '?c))
+
+(define (constant? x)
+  (number? x))
+
+(define (arbitary-variable? pat)
+  (eq? (car pat) '?v))
+
+(define (variable? x)
+  (and (atom? x)
+       (not (number? x))))
+
+(define (arbitary-expression? pat)
+  (eq? (car pat) '?))
+
+(define (skeleton-evaluation? s)
+  (eq? (car s) ':))
+
+(define (skeleton-evaluate s dict)
+  (cdr (assoc s dict)))
