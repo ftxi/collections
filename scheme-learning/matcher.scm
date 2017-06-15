d7db185ea4fedcc1226b77c4c7b11cc1aa9abb84
ss <1307689960@qq.com>
2017-06-15 16:35:54 +0800

    simplifer

--- scheme-learning/matcher.scm
+++ scheme-learning/matcher.scm
@@ -4,23 +4,6 @@
   (and (not (pair? x))
        (not (null? x))))
 
-(define deriv-rules
-  '(((diff (?c c) (?v v)) 0)
-    ((diff (?v v) (?v v)) 1)
-    ((diff (?v u) (?v v)) 0)
-    ((diff (+ (? x1) (? x2)) (?v v))
-     (+ (diff (: x1) (: v)) (diff (: x2) (: v))))
-    ((diff (* (? x1) (? x2)) (?v v))
-     (+ (* (diff (: x1) (: v)) (: x2))
-        (* (diff (: x2) (: v)) (: x1))))))
-
-#|
-(define algebra-rules
-  '(
-    (((? op) (?c c1) (?c c2)) (: (op c1 c2)))
-    (((? op) (? e) (?c c)) ((: op) (: c) (: e)))
-|#
-
 (define (match pat exp dict)
   (cond
     ((eq? dict 'failed) 'failed)
@@ -29,14 +12,22 @@
               (eq? pat exp))
          dict
          'failed))
-    ((null? pat)
-     dict)
+    ((null? pat) dict)
+    ((null? exp) 'failed)
     ((arbitary-constant? pat)
      (if (constant? exp)
          (extend-dict pat exp dict)
          'failed))
     ((arbitary-variable? pat)
      (if (variable? exp)
+         (extend-dict pat exp dict)
+         'failed))
+    ((arbitary-atom? pat)
+     (if (atom? exp)
+         (extend-dict pat exp dict)
+         'failed))
+    ((arbitary-pair? pat)
+     (if (pair? exp)
          (extend-dict pat exp dict)
          'failed))
     ((arbitary-expression? pat)
@@ -69,6 +60,27 @@
             ((equal? (cdr v) exp) dict)
             (else 'failed)))))
 
+(define (simplifier the-rules)
+  (define (simplify-exp exp)
+    (try-rules (if (pair? exp)
+                   (map simplify-exp exp)
+                   exp)))
+  (define (try-rules exp)
+    (define (scan rules)
+      (if (null? rules)
+          exp
+          (let ((dict (match (pattern-of (car rules))
+                             exp
+                             '())))
+            (if (eq? dict 'failed)
+                (scan (cdr rules))
+                (simplify-exp
+                 (instantiate
+                     (skeleton-of (car rules))
+                     dict))))))
+    (scan the-rules))
+  simplify-exp)
+
 (define (arbitary-constant? pat)
   (eq? (car pat) '?c))
 
@@ -81,6 +93,12 @@
 (define (variable? x)
   (and (atom? x)
        (not (number? x))))
+
+(define (arbitary-atom? pat)
+  (eq? (car pat) '?a))
+
+(define (arbitary-pair? pat)
+  (eq? (car pat) '?p))
 
 (define (arbitary-expression? pat)
   (eq? (car pat) '?))
@@ -99,4 +117,37 @@
         (cdr t)
         s)))
 
+(define (pattern-of rule)
+  (car rule))
+
+(define (skeleton-of rule)
+  (cadr rule))
+
+;; --------------------------
+
+
+(define derivative-rules
+  '(((diff (?c c) (?v v)) 0)
+    ((diff (?v v) (?v v)) 1)
+    ((diff (?v u) (?v v)) 0)
+    ((diff (+ (? x1) (? x2)) (?v v))
+     (+ (diff (: x1) (: v)) (diff (: x2) (: v))))
+    ((diff (* (? x1) (? x2)) (?v v))
+     (+ (* (diff (: x1) (: v)) (: x2))
+        (* (diff (: x2) (: v)) (: x1))))))
+
+(define algebra-rules
+  '(((+ (?p e) (?a a))
+     (+ (: a) (: e)))
+    ((* (?p e) (?a a))
+     (* (: a) (: e)))
+    ((+ 0 (? e)) (: e))
+    ((* 1 (? e)) (: e))
+    ((* 0 (? e)) 0)
+    ((* (? a) (+ (? b) (? c)))
+     (+ (* (: a) (: b)) (* (: a) (: c))))))
+
+(define dsimp
+  (simplifier (append algebra-rules
+                      derivative-rules)))
 
