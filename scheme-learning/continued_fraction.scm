3050ad72b3c63c733c3af54c82b11bb84a7e7950
ss <1307689960@qq.com>
2017-08-17 20:04:06 +0800

    continued fraction

---
+++ scheme-learning/continued_fraction.scm
@@ -0,0 +1,29 @@
+(load "stream.scm")
+
+(define (continued-fraction n)
+  (let* ((m (inexact->exact (floor n)))
+         (p (- n m)))
+    (if (= p 0)
+        (lazy-cons m '())
+        (lazy-cons m (continued-fraction (/ p))))))
+
+(define (cfrac k n)
+  (take k (continued-fraction n)))
+
+(define (approximate k n)
+  (let ((rcf (reverse (cfrac k n))))
+    (let loop ((acc (car rcf)) (l (cdr rcf)))
+      (if (null? l)
+          acc
+          (loop (+ (/ acc) (car l)) (cdr l))))))
+
+(define (gradually-approximate k n)
+  (let loop ((u k) (ans '()))
+    (if (= u 0)
+        ans
+        (loop (- u 1)
+              (cons (approximate u n) ans)))))
+
+;;; some useful constants
+(define e (exp 1))
+(define pi (* 4 (atan 1)))
